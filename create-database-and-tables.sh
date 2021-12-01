8989fc6a6e5bea03edc8f512f4ce5e487b9bc52c
lohithroydevops <95010251+lohithroydevops@users.noreply.github.com>
2021-11-30 23:30:08 -0700

    Update create-database-and-tables.sh

--- create-database-and-tables.sh
+++ create-database-and-tables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-PGPASSWORD=password123 /usr/local/bin/psql -v ON_ERROR_STOP=1 -U demo_user -d demo -h postgresdb -p 5432 <<-EOSQL
+/usr/local/bin/psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" -h postgresdb -p 5432 <<-EOSQL
   BEGIN;
    CREATE SCHEMA demo_user;
    CREATE TABLE demo_user.store( id SERIAL PRIMARY KEY, name VARCHAR (50));
